ddef2b679425a9244edff8fe5cb09ecaacffb2f1
TGMhfock <hagen.fock@gmail.com>
2014-09-24 15:22:11 +0200

    Assembler & Supplier 0.1

---
+++ src/Assembler.java
@@ -0,0 +1,28 @@
+
+public class Assembler implements Runnable {
+	private Part[] storage;
+	
+	public Assembler() {
+		
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
+	private Part requestPart(PartType type) {
+		return null;
+	}
+	private void returnParts(){
+		
+	}
+	private void sort(Part type) {
+		
+	}
+	/*
+	 * http://docs.oracle.com/javase/7/docs/api/java/util/Arrays.html
+	 */
+	private void robotArchive(Part[] parts){
+		
+	}
+}
---
+++ src/Supplier.java
@@ -0,0 +1,14 @@
+
+public class Supplier implements Runnable {
+	
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public int[] genNumbers() {
+		return null;
+	}
+}
