1c62c1caa94dcc043c5a6ac48280afc25397c266
TGMhfock <hagen.fock@gmail.com>
2014-09-25 17:06:58 +0200

    added comments

--- src/tgm/sew/hit/roboterfabrik/Assembler.java
+++ src/tgm/sew/hit/roboterfabrik/Assembler.java
@@ -11,12 +11,25 @@ public class Assembler implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 	}
+	/**
+	 * Requests a PartType from the StorageGuy
+	 * @param type
+	 * @return 
+	 */
 	private Part requestPart(PartType type) {
 		return null;
 	}
+	/**
+	 * returns Parts back to the StorageGuy if the 
+	 * Assembler didnt got all requierd Parts
+	 */
 	private void returnParts(){
 		
 	}
+	/**
+	 * sorts numbers as a symbol of assembling
+	 * @param type
+	 */
 	private void sort(Part type) {
 		
 	}
