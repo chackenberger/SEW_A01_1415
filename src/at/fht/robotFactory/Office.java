cf19dd660b9bf75efdf1a7aafb7d3c2a0bf5d8cb
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2014-09-27 16:26:03 +0200

    Added the missing attributes from the class diagram

--- src/at/fht/robotFactory/Office.java
+++ src/at/fht/robotFactory/Office.java
@@ -2,22 +2,27 @@ package at.fht.robotFactory;
 
 import org.apache.logging.log4j.Logger;
 
+import com.sun.swing.internal.plaf.synth.resources.synth;
+
 public class Office
 {
 	private Logger logger;
+	
+	private long employeeID;
+	private long threadeeID;
 	
 	public Office()
 	{
 		
 	}
 	
-	public void requestID()
+	public synchronized long requestID()
 	{
-		
+		return 0;
 	}
 	
-	public void requestRobot()
+	public synchronized long requestRobotID()
 	{
-		
+		return 0;
 	}
 }
