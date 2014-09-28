e14ddc28ef6ae35000dc45b24e5059a8aa2d0a9e
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2014-09-28 13:42:27 +0200

    All attributes in Factory are now static

--- src/at/fht/robotFactory/Factory.java
+++ src/at/fht/robotFactory/Factory.java
@@ -4,11 +4,17 @@ public class Factory implements Runnable {
 
 	public static Factory instance;
 	
-	private Supplier[] suppliers;
-	private Assembler[] assemblers;
+	private static Supplier[] suppliers;
+	private static Assembler[] assemblers;
 	
 	private static Office office = new Office();
 	private static Storageguy storage;
+	
+	private static Logger logger;
+	
+	public static void main(String[] args) {
+		
+	}
 	
 	@Override
 	public void run() {
