c80e163b0ab139730d290059f974c9a05db4ce6b
seyyidTiryaki <seyyid.tiryaki@gmail.com>
2014-09-29 21:44:44 +0200

    commit

--- src/at/fht/robotFactory/Office.java
+++ src/at/fht/robotFactory/Office.java
@@ -42,3 +42,47 @@ public class Office
 		return threadeeID;
 	}
 }
+package at.fht.robotFactory;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/*
+ * @author Seyyid Tiryaki
+ * @description The Office awards IDs for the employees and robots
+ */
+
+public class Office
+{
+	private Logger logger;
+	
+	private long employeeID;
+	private long threadeeID;
+	
+	public Office()
+	{
+		logger = LogManager.getLogger(this.getClass().getName());
+		employeeID = 0;
+		threadeeID = 0;
+		logger.info("Office generated");
+	}
+	/*
+	 * @return ID for employee
+	 */
+	public synchronized long requestID()
+	{	
+		employeeID++;
+		logger.info("request ID for employee: " +  employeeID);
+		return employeeID;
+		
+	}
+	
+	/*
+	 * @return ID for Robot
+	 */
+	public synchronized long requestRobotID()
+	{
+		employeeID++;
+		logger.info("request ID for robot: " +  employeeID);
+		return threadeeID;
+	}
+}
