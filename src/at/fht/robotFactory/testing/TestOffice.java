65b2d6fe96bac48253c0da99e12e1f295e10b5bf
seyyidTiryaki <seyyid.tiryaki@gmail.com>
2014-09-29 14:24:22 +0200

    Test Office

--- src/at/fht/robotFactory/testing/TestOffice.java
+++ src/at/fht/robotFactory/testing/TestOffice.java
@@ -7,21 +7,27 @@ import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import at.fht.robotFactory.Part;
+import at.fht.robotFactory.PartType;
+import at.fht.robotFactory.Assembler;
 import at.fht.robotFactory.Office;
 
 public class TestOffice {
 
-	Office c;
-	@Before
-	public void setup() {
+	private Office c;
+	private Assembler as;
 	
+	@Before
+	public void setup()
+	{
 		c = new Office();
+		as = new Assembler();
 	}
 	
 	@Test
-	public void testRequestID{
-		
+	public void testRequestID() {
+		as.setID(c.requestID());
 	}
 
 }
-
