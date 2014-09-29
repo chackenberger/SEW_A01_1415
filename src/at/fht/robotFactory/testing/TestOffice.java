c0de02f2fc45e97692e7d6c7f42dcf16e54b3b30
seyyidTiryaki <seyyid.tiryaki@gmail.com>
2014-09-29 14:20:46 +0200

    Test Office

---
+++ src/at/fht/robotFactory/testing/TestOffice.java
@@ -0,0 +1,27 @@
+package at.fht.robotFactory.testing;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+import at.fht.robotFactory.Office;
+
+public class TestOffice {
+
+	Office c;
+	@Before
+	public void setup() {
+	
+		c = new Office();
+	}
+	
+	@Test
+	public void testRequestID{
+		
+	}
+
+}
+
