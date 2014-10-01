80b7f280c26f75a3937cc29609ee1a541be991ac
Christoph Hackenberger <chackenberger@student.tgm.ac.at>
2014-10-01 16:44:04 +0200

    Added more testing for storageguy

--- src/at/fht/robotFactory/testing/TestStorageguy.java
+++ src/at/fht/robotFactory/testing/TestStorageguy.java
@@ -3,10 +3,12 @@ package at.fht.robotFactory.testing;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import at.fht.robotFactory.Factory;
 import at.fht.robotFactory.Part;
 import at.fht.robotFactory.PartType;
 import at.fht.robotFactory.Storageguy;
@@ -17,18 +19,27 @@ public class TestStorageguy {
 	
 	@Before
 	public void setup() {
+		Factory.testMain("-a 10 -l lager -o logs/log --lieferanten 5 -t 20000");
 		try {
 			File dir = new File("/Users/Christoph/Desktop/stguy");
 			for (File f : dir.listFiles())
 				f.delete();
 			dir.delete();
 		}catch (Exception ex){}
-		g = new Storageguy("/Users/Christoph/Desktop/stguy");
+		try {
+			g = new Storageguy("/Users/Christoph/Desktop/stguy");
+		} catch (IOException e) {
+		}
 	}
 	
 	@Test
 	public void testCreateDirs() {
-		new Storageguy("/Users/Christoph/Desktop/stguy");
+		try {
+			new Storageguy("/Users/Christoph/Desktop/stguy");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Test
@@ -68,4 +79,36 @@ public class TestStorageguy {
 		
 		g.storeThreadee(1, ps);
 	}
+	
+	@Test(expected=IOException.class)
+	public void testInvalidStorageDir() throws IOException {
+		new Storageguy("//test");
+	}
+	
+	@Test
+	public void testTryReadingPartButDeleteFiles() {
+		//setup
+		Part[] ps = new Part[PartType.values().length];
+		int i = 0;
+		for(PartType type : PartType.values()) {
+			Part p = new Part(type, new int[]{0,1,2,3,4});
+			ps[i] = p;
+			i++;
+			g.storePart(p);
+		}
+		
+		//now delete Files
+		try {
+			File dir = new File("/Users/Christoph/Desktop/stguy");
+			for (File f : dir.listFiles())
+				f.delete();
+			dir.delete();
+		}catch (Exception ex){}
+		
+		assertEquals(null, g.getPart(ps[0].getPartType()));
+		assertEquals(null, g.getPart(ps[1].getPartType()));
+		assertEquals(null, g.getPart(ps[2].getPartType()));
+		assertEquals(null, g.getPart(ps[3].getPartType()));
+	}
+	
 }
